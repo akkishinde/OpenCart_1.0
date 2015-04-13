f5111460c7e8d6fbf4e62de5bb8abe5fe2b804eb
akshay <akshay11.shinde@gmail.com>
2015-04-13 13:00:24 +0530

    Out Of Stock verification

--- app/src/main/java/info/androidhive/Mahaveer/ItemsDetails.java
+++ app/src/main/java/info/androidhive/Mahaveer/ItemsDetails.java
@@ -38,38 +38,40 @@ import java.util.ArrayList;
 /**
  * Created by Splash New on 3/31/2015.
  */
-public class ItemsDetails extends Activity{
+public class ItemsDetails extends Activity {
     private ProgressDialog pDialog;
-    NetworkImageView item_header,im1,im2,im3,im4;
-    ImageButton addcart,addwish;
-    TextView title,manufacturer,item_name,brand,stock,price,description;
+    NetworkImageView item_header, im1, im2, im3, im4;
+    ImageButton addcart, addwish;
+    TextView title, manufacturer, item_name, brand, stock, price, description;
     EditText qty;
     ArrayList<String> imageadapter = new <String>ArrayList<String>();
     private static String pid;
     ImageLoader imageLoader = Session.getInstance().getImageLoader();
-    String url="http://webshop.opencart-api.com/api/rest/products/";
-    static String headurl="";
-    static String item_id="";
-    static String item_qty="";
+    String url = "http://webshop.opencart-api.com/api/rest/products/";
+    static String headurl = "";
+    static String item_id = "";
+    static String item_qty = "";
+     int empty_flag = 0;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_details);
-        title= (TextView) findViewById(R.id.name);
-        manufacturer=(TextView) findViewById(R.id.manufacturer);
-        item_header= (NetworkImageView) findViewById(R.id.thumbnail);
-        im1=(NetworkImageView) findViewById(R.id.imageView2);
-        im2=(NetworkImageView) findViewById(R.id.imageView3);
-        im3=(NetworkImageView) findViewById(R.id.imageView4);
-        im4=(NetworkImageView) findViewById(R.id.imageView5);
-        item_name=(TextView)findViewById(R.id.item_name);
-        brand=(TextView)findViewById(R.id.brand_name);
-        stock=(TextView)findViewById(R.id.stock_status);
-        price=(TextView)findViewById(R.id.price);
-        description=(TextView)findViewById(R.id.description);
-        addcart=(ImageButton)findViewById(R.id.addCartButton);
-        addwish=(ImageButton)findViewById(R.id.addWishButton);
-        qty=(EditText)findViewById(R.id.editText);
+        title = (TextView) findViewById(R.id.name);
+        manufacturer = (TextView) findViewById(R.id.manufacturer);
+        item_header = (NetworkImageView) findViewById(R.id.thumbnail);
+        im1 = (NetworkImageView) findViewById(R.id.imageView2);
+        im2 = (NetworkImageView) findViewById(R.id.imageView3);
+        im3 = (NetworkImageView) findViewById(R.id.imageView4);
+        im4 = (NetworkImageView) findViewById(R.id.imageView5);
+        item_name = (TextView) findViewById(R.id.item_name);
+        brand = (TextView) findViewById(R.id.brand_name);
+        stock = (TextView) findViewById(R.id.stock_status);
+        price = (TextView) findViewById(R.id.price);
+        description = (TextView) findViewById(R.id.description);
+        addcart = (ImageButton) findViewById(R.id.addCartButton);
+        addwish = (ImageButton) findViewById(R.id.addWishButton);
+        qty = (EditText) findViewById(R.id.editText);
         ActionBar mActionBar = getActionBar();
         assert mActionBar != null;
         mActionBar.setDisplayHomeAsUpEnabled(true);
@@ -103,7 +105,7 @@ public class ItemsDetails extends Activity{
 
             @Override
             public void onClick(View v) {
-            item_header.setImageUrl(imageadapter.get(0), imageLoader);
+                item_header.setImageUrl(imageadapter.get(0), imageLoader);
             }
         });
         im3.setOnClickListener(new View.OnClickListener() {
@@ -127,48 +129,68 @@ public class ItemsDetails extends Activity{
                 item_header.setImageUrl(headurl, imageLoader);
             }
         });
+
         addcart.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+                if (empty_flag == 0) {
+                    item_qty = qty.getText().toString();
+                    StringEntity entity = null;
+                    JSONObject innerObj = new JSONObject();
+                    JSONObject outerObj = new JSONObject();
+                    try {
+                        innerObj.put(item_id, item_qty);
+                        outerObj.put("quantity", innerObj);
+                        entity = new StringEntity(outerObj.toString());
 
-                item_qty=qty.getText().toString();
-                StringEntity entity = null;
-                JSONObject innerObj = new JSONObject();
-                JSONObject outerObj = new JSONObject();
-                try {
-                    innerObj.put(item_id, item_qty);
-                    outerObj.put("quantity",innerObj);
-                    entity = new StringEntity(outerObj.toString());
+                    } catch (JSONException | UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                    LoginActivity.client.put(getApplicationContext(), "http://webshop.opencart-api.com/api/rest/cart", entity, "application/json", new AsyncHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(String response) {
+                            View v;
+                            Toast toast;
+                            TextView text;
+                            toast = Toast.makeText(getApplicationContext(), "Item added to Cart!", Toast.LENGTH_SHORT);
+                            v = toast.getView();
+                            text = (TextView) v.findViewById(android.R.id.message);
+                            text.setTextColor(getResources().getColor(R.color.mWhite));
+                            text.setShadowLayer(0, 0, 0, 0);
+                            v.setBackgroundResource(R.color.mTeal);
+                            toast.show();
+                            title.setFocusable(true);
+                            title.setFocusableInTouchMode(true);
+                            qty.setText("");
 
-                } catch (JSONException | UnsupportedEncodingException e) {
-                    e.printStackTrace();
+
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Throwable error,
+                                              String content) {
+                            Toast.makeText(getApplicationContext(), "Something went wrong at Server Side! " + statusCode, Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }
-                LoginActivity.client.put(getApplicationContext(),"http://webshop.opencart-api.com/api/rest/cart", entity, "application/json", new AsyncHttpResponseHandler(){
-                    @Override
-                    public void onSuccess(String response) {
-                        View v;
-                        Toast toast;
-                        TextView text;
-                        toast=Toast.makeText(getApplicationContext(), "Item added to Cart!", Toast.LENGTH_SHORT);
-                        v = toast.getView();
-                        text = (TextView) v.findViewById(android.R.id.message);
-                        text.setTextColor(getResources().getColor(R.color.mWhite));
-                        text.setShadowLayer(0,0,0,0);
-                        v.setBackgroundResource(R.color.mTeal);
-                        toast.show();
-                        title.setFocusable(true);
-                        title.setFocusableInTouchMode(true);
-                        qty.setText("");
+                else
+                {
 
+                    Toast toast;
+                    TextView text;
+                    toast = Toast.makeText(getApplicationContext(), "Out Of Stock", Toast.LENGTH_SHORT);
+                    v = toast.getView();
+                    text = (TextView) v.findViewById(android.R.id.message);
+                    text.setTextColor(getResources().getColor(R.color.mWhite));
+                    text.setShadowLayer(0, 0, 0, 0);
+                    v.setBackgroundResource(R.color.mRed);
+                    toast.show();
+                    title.setFocusable(true);
+                    title.setFocusableInTouchMode(true);
+                    qty.setText("");
 
-                    }
-                    @Override
-                    public void onFailure(int statusCode, Throwable error,
-                                          String content) {
-                        Toast.makeText(getApplicationContext(),"Something went wrong at Server Side! "+statusCode,Toast.LENGTH_SHORT).show();
-                    }
-                });
+                }
             }
         });
 
@@ -208,11 +230,11 @@ public class ItemsDetails extends Activity{
     }
 
     private void LoadData() {
-        url="http://webshop.opencart-api.com/api/rest/products/"+pid;
-        AsyncHttpClient client=new AsyncHttpClient();
-        client.addHeader("X-Oc-Merchant-Id","123");
-        client.addHeader("X-Oc-Merchant-Language","en");
-        client.get(getApplicationContext(),url, new AsyncHttpResponseHandler() {
+        url = "http://webshop.opencart-api.com/api/rest/products/" + pid;
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.addHeader("X-Oc-Merchant-Id", "123");
+        client.addHeader("X-Oc-Merchant-Language", "en");
+        client.get(getApplicationContext(), url, new AsyncHttpResponseHandler() {
             public static final String TAG = "";
 
             @Override
@@ -226,34 +248,37 @@ public class ItemsDetails extends Activity{
                         JSONObject json2 = json.getJSONObject("data");
                         //Toast.makeText(getApplicationContext(), "title:", Toast.LENGTH_SHORT).show();
                         title.setText(json2.getString("name"));
-                        item_id=json2.getString("id");
+                        item_id = json2.getString("id");
                         manufacturer.setText(json2.getString("manufacturer"));
                         item_header.setImageUrl(json2.getString("image"), imageLoader);
-                        headurl=json2.getString("image");
-                        im1.setImageUrl(json2.getString("image"),imageLoader);
-                        JSONArray jsonArray=json2.getJSONArray("images");
-                        for(int i=0;i<jsonArray.length();i++) {
-                          imageadapter.add(jsonArray.get(i).toString());
+                        headurl = json2.getString("image");
+                        im1.setImageUrl(json2.getString("image"), imageLoader);
+                        JSONArray jsonArray = json2.getJSONArray("images");
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            imageadapter.add(jsonArray.get(i).toString());
                         }
-                        if(imageadapter.size()>0) {
+                        if (imageadapter.size() > 0) {
                             im2.setEnabled(true);
                             im2.setVisibility(View.VISIBLE);
                             im2.setImageUrl(imageadapter.get(0), imageLoader);
                         }
-                        if(imageadapter.size()>1) {
+                        if (imageadapter.size() > 1) {
                             im3.setEnabled(true);
                             im3.setVisibility(View.VISIBLE);
                             im3.setImageUrl(imageadapter.get(1), imageLoader);
                         }
-                        if(imageadapter.size()>2) {
+                        if (imageadapter.size() > 2) {
                             im4.setEnabled(true);
                             im4.setVisibility(View.VISIBLE);
                             im4.setImageUrl(imageadapter.get(2), imageLoader);
                         }
                         item_name.setText(json2.getString("name"));
-                        brand.setText("Manufacturer: "+json2.getString("manufacturer"));
+                        brand.setText("Manufacturer: " + json2.getString("manufacturer"));
                         stock.setText("Stock: " + json2.getString("stock_status"));
-                        price.setText("Price: "+json2.getString("price"));
+                        if (json2.getString("stock_status").equals("Out Of Stock")) {
+                            empty_flag = 1;
+                        }
+                        price.setText("Price: " + json2.getString("price"));
                         description.setText(json2.getString("description").replaceAll("<(.*?)\\>", " ")
                                 .replaceAll("<(.*?)\\\n", " ")
                                 .replaceFirst("(.*?)\\>", " ")
@@ -273,7 +298,7 @@ public class ItemsDetails extends Activity{
             public void onFailure(int statusCode, Throwable error,
                                   String content) {
                 pDialog.hide();
-                Toast.makeText(getApplicationContext(),"Something went wrong at server side!",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Something went wrong at server side!", Toast.LENGTH_SHORT).show();
             }
 
         });
@@ -299,7 +324,6 @@ public class ItemsDetails extends Activity{
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // toggle nav drawer on selecting action bar app icon/title
-
 
 
         // Handle action bar actions click
@@ -329,6 +353,7 @@ public class ItemsDetails extends Activity{
         Intent intent = new Intent(this, ViewCart.class);
         startActivity(intent);
     }
+
     private void CallViewHistory() {
         Intent intent = new Intent(this, OrderHistory.class);
         startActivity(intent);
