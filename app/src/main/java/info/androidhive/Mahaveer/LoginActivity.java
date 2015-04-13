03432cd8302a5745d24975fa788643b3f2d44341
akshay <akshay11.shinde@gmail.com>
2015-04-13 12:36:09 +0530

    Save User Credentials.

--- app/src/main/java/info/androidhive/Mahaveer/LoginActivity.java
+++ app/src/main/java/info/androidhive/Mahaveer/LoginActivity.java
@@ -3,7 +3,9 @@ package info.androidhive.Mahaveer;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
@@ -35,12 +37,39 @@ public class LoginActivity extends Activity{
     private static final String TAG = "";
     EditText username,password;
     ProgressDialog prgDialog;
+    private static final String PREFS_NAME = "preferences";
+    private static final String PREF_UNAME = "username";
+    private static final String PREF_PASSWORD = "password";
+    private final String DefaultUnameValue = "";
+    private String UnameValue;
+    private final String DefaultPasswordValue = "";
+    private String PasswordValue;
+    private void loadPreferences() {
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME,
+                Context.MODE_PRIVATE);
 
+        // Get value
+        UnameValue = settings.getString(PREF_UNAME, DefaultUnameValue);
+        PasswordValue = settings.getString(PREF_PASSWORD, DefaultPasswordValue);
+        username.setText(UnameValue);
+        password.setText(PasswordValue);
+    }
+    private void savePreferences() {
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME,
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        UnameValue = (username.getText()).toString();
+        PasswordValue = (password.getText()).toString();
+        editor.putString(PREF_UNAME, UnameValue);
+        editor.putString(PREF_PASSWORD, PasswordValue);
+        editor.apply();
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
         ActionBar mActionBar = getActionBar();
         assert mActionBar != null;
         mActionBar.setBackgroundDrawable(new ColorDrawable(getResources()
@@ -63,7 +92,7 @@ public class LoginActivity extends Activity{
         password=(EditText)findViewById(R.id.password_text);
         prgDialog = new ProgressDialog(this);
         prgDialog.setMessage("Please wait...");
-
+        loadPreferences();
 
     }
     public void login(View view)
@@ -139,6 +168,7 @@ public class LoginActivity extends Activity{
                 try {
                     JSONObject obj = new JSONObject(response);
                     if (obj.getString("success").equals("true")) {
+                        savePreferences();
                         prgDialog.hide();
                         JSONObject json= (JSONObject) new JSONTokener(response).nextValue();
                         JSONObject json2 = json.getJSONObject("data");
